c17e0aa5041555684c49918de8769e3668b60d1e
Zachary Juang <zachary822@me.com>
2024-09-21 15:11:16 -0400

    more better main

---
+++ src/Network/GPSD.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Network.GPSD where
+
+import Control.Exception qualified as E
+import Control.Monad
+import Control.Monad.State.Strict
+import Data.Aeson
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
+import Data.List
+import Data.List.NonEmpty qualified as NE
+import Data.Maybe
+import Network.GPSD.Types
+import Network.Socket
+import Network.Socket.ByteString
+
+getRow :: Socket -> StateT ByteString IO [Maybe Info]
+getRow s = do
+  buf <- get
+  msg <- liftIO $ recv s 4096
+  case unsnoc (B.split 10 (buf <> msg)) of
+    Just (xs, l) -> do
+      put l
+      return (map (decode . B.fromStrict) xs)
+    Nothing -> return []
+
+processMessage :: (Info -> IO ()) -> Socket -> IO ()
+processMessage f s = do
+  msg <- recv s 4096
+  case decode (B.fromStrict msg) of
+    Just info -> do
+      f info
+      sendAll s "?WATCH={\"enable\":true,\"json\":true}"
+
+      evalStateT
+        ( forever $ do
+            msgs <- getRow s
+            liftIO $ traverse f (catMaybes msgs)
+        )
+        ""
+    Nothing -> fail "fail to initialize connection"
+
+runTCPClient :: HostName -> ServiceName -> (Socket -> IO a) -> IO a
+runTCPClient host port client = do
+  addr <- resolve
+  E.bracket (open addr) close client
+ where
+  resolve = do
+    let hints = defaultHints{addrSocketType = Stream}
+    NE.head <$> getAddrInfo (Just hints) (Just host) (Just port)
+  open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
+    connect sock $ addrAddress addr
+    return sock
+
+runGPSDClient :: HostName -> ServiceName -> (Info -> IO ()) -> IO ()
+runGPSDClient host port = runTCPClient host port . processMessage
---
+++ src/Network/GPSD/Types.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Network.GPSD.Types where
+
+import Data.Aeson
+import Data.Text
+import Data.Time.Clock
+import GHC.Generics (Generic)
+
+data Satellite = Satellite
+  { prn :: Int
+  , el :: Double
+  , az :: Double
+  , ss :: Double
+  , used :: Bool
+  , gnssid :: Int
+  , svid :: Int
+  }
+  deriving (Generic, Show, Eq)
+
+instance FromJSON Satellite where
+  parseJSON = withObject "Satellite" $ \o ->
+    Satellite
+      <$> o .: "PRN"
+      <*> o .: "el"
+      <*> o .: "az"
+      <*> o .: "ss"
+      <*> o .: "used"
+      <*> o .: "gnssid"
+      <*> o .: "svid"
+
+data Info
+  = VersionInfo
+      { release :: Text
+      , rev :: Text
+      , proto_major :: Int
+      , proto_minor :: Int
+      }
+  | DevicesInfo
+      { devices :: [Info]
+      }
+  | DeviceInfo
+      { path :: Text
+      , driver :: Text
+      , subtype :: Text
+      , activated :: UTCTime
+      , flags :: Int
+      , native :: Int
+      , bps :: Int
+      , parity :: Text
+      , stopbits :: Int
+      , cycle :: Double
+      , mincycle :: Double
+      }
+  | TPVInfo
+      { device :: Text
+      , status :: Int
+      , mode :: Int
+      , time :: UTCTime
+      , ept :: Double
+      , lat :: Double
+      , lon :: Double
+      , altHAE :: Double
+      , altMSL :: Double
+      , alt :: Double
+      , epx :: Double
+      , epy :: Double
+      , epv :: Double
+      , track :: Double
+      , magtrack :: Double
+      , magvar :: Double
+      , speed :: Double
+      , climb :: Double
+      , eps :: Double
+      , epc :: Double
+      , geoidSep :: Double
+      , eph :: Double
+      , sep :: Double
+      }
+  | SkyInfo
+      { device :: Text
+      , xdop :: Double
+      , ydop :: Double
+      , vdop :: Double
+      , tdop :: Double
+      , hdop :: Double
+      , gdop :: Double
+      , pdop :: Double
+      , nSat :: Int
+      , uSat :: Int
+      , satellites :: [Satellite]
+      }
+  deriving (Generic, Show, Eq)
+
+options :: Options
+options =
+  defaultOptions
+    { sumEncoding = UntaggedValue
+    }
+
+instance FromJSON Info where
+  parseJSON = genericParseJSON options
